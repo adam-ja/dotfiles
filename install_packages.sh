ca09cb928e9c0d49418555c5cad7326dd410f05b
Adam Auskerin <adam.auskerin@gmail.com>
2015-05-23 20:16:31 +0100

    Added oh-my-zsh, mapped caps lock to escape

--- install_packages.sh
+++ install_packages.sh
@@ -3,13 +3,14 @@
 # Install useful apt packages
 sudo apt-get install --assume-yes \
     curl \
-    tree \
+    gsettings \
     htop \
-    python-pip \
-    openssh-server \
     network-manager-openvpn \
-    zsh \
+    openssh-server \
+    python-pip \
     tmux \
+    tree \
+    zsh \
 
 # Install joe - a .gitignore generator
 pip install joe
