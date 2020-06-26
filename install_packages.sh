5c4f47f63de612f0bd7034bc0e764a0ab30d784f
Adam Auskerin <adam.auskerin@gmail.com>
2020-06-26 14:37:48 +0100

    Remove unused packages from install

--- install_packages.sh
+++ install_packages.sh
@@ -3,11 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
 
 # Install useful apt packages
-PACKAGES='curl htop jq network-manager-openvpn openssh-server python-pip tmux tree zsh'
+PACKAGES='curl htop jq openssh-server tmux tree zsh'
 sudo apt install --assume-yes ${PACKAGES}
-
-# Install joe - a .gitignore generator
-pip install joe
 
 # Install ripgrep (rg) - a much faster alternative to grep, ag, etc
 ${DIR}/install_ripgrep.sh
