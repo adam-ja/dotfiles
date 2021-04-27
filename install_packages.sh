abdd316c74860662831db7687dfe0dcf359a3def
Adam Auskerin <adam.auskerin@gmail.com>
2021-04-27 12:05:55 +0100

    Install ripgrep via apt

--- install_packages.sh
+++ install_packages.sh
@@ -3,14 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
 
 # Install useful apt packages
-PACKAGES='curl htop jq openssh-server php tmux tree zsh'
+PACKAGES='curl htop jq openssh-server php ripgrep tmux tree zsh'
 sudo apt install --assume-yes ${PACKAGES}
 
 # Install node as a snap (includes npm and yarn)
 sudo snap install node
-
-# Install ripgrep (rg) - a much faster alternative to grep, ag, etc
-${DIR}/install_ripgrep.sh
 
 # Install bat - a cat clone with syntax highlighting and git integration
 ${DIR}/install_bat.sh
