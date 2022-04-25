57fe60fe8003531f8ebdfcab95d1fbe7b669e364
Adam Auskerin <adam.auskerin@gmail.com>
2022-04-25 11:52:57 +0100

    Improve fnm installation
    - Add auto-switch when version specified in directory
    - Skip shell setup during installation. The shell setup in the install
      script adds `eval "$(fnm env)"`, without the auto-switch, to whichever
      shell is default when the script runs. My .zshrc already includes all
      it needs.

--- install_node.sh
+++ install_node.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
 
 # First, install fnm
-curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir $HOME/.local/bin
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell --install-dir $HOME/.local/bin
 
 # Create/update zsh completions for fnm
 $HOME/.local/bin/fnm completions --shell zsh > ${DIR}/zsh/completions/_fnm
