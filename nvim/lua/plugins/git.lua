cb803de1351438a3a6a3f5a155c05392a1d13d99
Adam Auskerin <adam.auskerin@gmail.com>
2023-05-17 13:02:09 +0100

    Add gitsigns keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -11,5 +11,95 @@ return {
             current_line_blame_formatter = '      <author>, <author_time:%Y-%m-%d>  <summary>',
             current_line_blame_formatter_nc = '      Not committed yet',
         },
+        event = 'BufRead',
+        keys = {
+            {
+                '<Leader>gn',
+                function()
+                    require('gitsigns').next_hunk()
+                end,
+                desc = 'Go to next hunk [gitsigns]',
+            },
+            {
+                '<Leader>gp',
+                function ()
+                    require('gitsigns').prev_hunk()
+                end,
+                desc = 'Go to previous hunk [gitsigns]',
+            },
+            {
+                '<Leader>gp',
+                -- TODO: make util function for pausing diagnostics hover (see lsp documentation keymap)
+                function ()
+                    require('gitsigns').preview_hunk()
+                end,
+                desc = 'Preview hunk [gitsigns]',
+            },
+            {
+                '<Leader>gtb',
+                function ()
+                    require('gitsigns').toggle_current_line_blame()
+                end,
+                desc = 'Toggle current line blame [gitsigns]',
+            },
+            {
+                '<Leader>gtd',
+                function ()
+                    require('gitsigns').toggle_deleted()
+                end,
+                desc = 'Toggle display of deleted lines [gitsigns]',
+            },
+            {
+                '<Leader>gs',
+                function ()
+                    require('gitsigns').stage_hunk()
+                end,
+                desc = 'Stage current hunk [gitsigns]',
+            },
+            {
+                '<Leader>gs',
+                function ()
+                    require('gitsigns').stage_hunk({vim.fn.line('.'), vim.fn.line('v')})
+                end,
+                desc = 'Stage selected lines from hunk [gitsigns]',
+                mode = 'v',
+            },
+            {
+                '<Leader>gS',
+                function ()
+                    require('gitsigns').stage_buffer()
+                end,
+                desc = 'Stage all hunks in buffer [gitsigns]',
+            },
+            {
+                '<Leader>gu',
+                function ()
+                    require('gitsigns').undo_stage_hunk()
+                end,
+                desc = 'Undo last stage hunk [gitsigns]',
+            },
+            {
+                '<Leader>gr',
+                function ()
+                    require('gitsigns').reset_hunk()
+                end,
+                desc = 'Reset current hunk [gitsigns]',
+            },
+            {
+                '<Leader>gr',
+                function ()
+                    require('gitsigns').reset_hunk({vim.fn.line('.'), vim.fn.line('v')})
+                end,
+                desc = 'Reset selected lines from hunk [gitsigns]',
+                mode = 'v',
+            },
+            {
+                '<Leader>gR',
+                function ()
+                    require('gitsigns').reset_buffer()
+                end,
+                desc = 'Reset all hunks in buffer [gitsigns]',
+            },
+        }
     },
 }
