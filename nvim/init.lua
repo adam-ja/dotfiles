a0a33a4f94a9e9b758740a039d2fa4ef58956b31
Adam Auskerin <adam.auskerin@gmail.com>
2024-07-08 18:57:45 +0100

    Add output when lazy.nvim bootstrap fails

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,7 +9,7 @@ vim.g.mapleader = ' '
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+  local output = vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
@@ -17,6 +17,16 @@ if not vim.loop.fs_stat(lazypath) then
     '--branch=stable', -- latest stable release
     lazypath,
   })
+  
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
