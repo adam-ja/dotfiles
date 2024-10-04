00dd99f4742fc189ec1188b4553e45e306a03001
Adam Auskerin <adam.auskerin@gmail.com>
2024-10-04 19:08:29 +0100

    Swap from tokyonight storm to moon
    Moon seems to have better contrast (at least for PHP)

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -16,7 +16,7 @@ return {
         config = function (_, opts)
             require('tokyonight').setup(opts)
 
-            vim.cmd('colorscheme tokyonight-storm')
+            vim.cmd('colorscheme tokyonight-moon')
             vim.opt.cursorline = true
         end,
     },
