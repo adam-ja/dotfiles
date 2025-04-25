5d9a86847c188e8847d980709085f60d700c60f8
Adam Auskerin <adam.auskerin@gmail.com>
2025-04-25 19:13:52 +0100

    Add keybindings for jumping to next/previous todo comments

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -143,5 +143,21 @@ return {
                 pattern = [[\b(KEYWORDS)\b]],
             },
         },
+        keys = {
+            {
+                ']t',
+                function()
+                    require('todo-comments').jump_next()
+                end,
+                desc = 'Next todo comment',
+            },
+            {
+                '[t',
+                function()
+                    require('todo-comments').jump_prev()
+                end,
+                desc = 'Previous todo comment',
+            }
+        }
     },
 }
