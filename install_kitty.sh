c34f64d56de894483aee8d1ae4be931baf6bdea2
Adam Auskerin <adam.auskerin@gmail.com>
2025-12-18 17:13:56 +0000

    Update kitty install script to not launch kitty after install

--- install_kitty.sh
+++ install_kitty.sh
@@ -4,7 +4,8 @@
 # Note symlinks are set up in `install.conf.yaml` so excluded here.
 
 # Download and install latest binary
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
+    launch=n # Don't launch kitty after installation
 
 if [[ "$(uname -s)" = "Linux" ]]; then
     # Desktop integration on linux
