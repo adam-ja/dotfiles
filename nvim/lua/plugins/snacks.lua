8345060e4117a46fb15c0010eb958b87960f4c1a
Adam Auskerin <adam.auskerin@gmail.com>
2026-01-23 17:10:31 +0000

    Enable gitbrowse snack

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -5,10 +5,20 @@ return { {
     opts = {
         bigfile = { enabled = true },
         dashboard = { enabled = true },
+        gitbrowse = { enabled = true },
         image = { enabled = true },
         indent = {
             enabled = true,
             scope = { enabled = false },
+        },
+    },
+    keys = {
+        {
+            '<Leader>gg',
+            function()
+                require('snacks').gitbrowse.open()
+            end,
+            desc = 'Open the remote page of the current file in the browser (e.g. on GitHub)',
         },
     },
 } }
